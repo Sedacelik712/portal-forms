f2983f89cb4ded4287cfceda6366ff28b8ef9c73
Sedacelik712 <38867574+Sedacelik712@users.noreply.github.com>
2018-12-28 00:06:53 +0300

    Add files via upload

---
+++ controllers/backend/FormsController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace kouosl\forms\controllers\backend;
+
+use Yii;
+use kouosl\forms\models\Forms;
+use kouosl\forms\models\FormsSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * FormsController implements the CRUD actions for Forms model.
+ */
+class FormsController extends \kouosl\base\controllers\backend\BaseController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Forms models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new FormsSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Forms model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Forms model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Forms();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->form_id]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Forms model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->form_id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Forms model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+   /* public function actionList($id) {
+        
+        $query = (new \yii\db\Query)->from($this->table.$id);
+        $form = FormModel::findModel($id);
+        $array = Json::decode($form->body);
+        
+        $merge_array = FormBase::onlyCorrectDataFields($array['body']);
+        
+        $dataProvider = new \yii\data\ActiveDataProvider(['query' => $query]);
+        
+         return $this->render('list', [
+         //   'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'only_data_fields' => ArrayHelper::getColumn($merge_array, 'name')
+        ]);
+    }
+    */
+    /**
+     * Finds the Forms model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Forms the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Forms::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
