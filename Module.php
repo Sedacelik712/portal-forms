264f25885b002990b3fea0f46f3358f3c78c5c5e
Sedacelik712 <38867574+Sedacelik712@users.noreply.github.com>
2019-01-01 19:10:25 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -12,50 +12,21 @@ use yii\web\HttpException;
 /**
  * forms module definition class
  */
-class Module extends \kouosl\base\Module
+class Module extends \yii\base\Module
 {
-    public $controllerNamespace = '';
-
+    public $controllerNamespace = 'kouosl\forms\controllers';
+    public $defaultRoute = 'forms';
+    public $table;
     public function init()
     {
         parent::init();
 
         // custom initialization code goes here
     }
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        switch ($this->namespace)
-        {
-            case 'backend':{
-
-            };break;
-            case 'frontend':{
-
-            };break;
-            case 'api':{
-
-                $behaviors['authenticator'] = [
-                    'class' => CompositeAuth::className(),
-                    'authMethods' => [
-                        HttpBasicAuth::className(),
-                        HttpBearerAuth::className(),
-                        QueryParamAuth::className(),
-                    ],
-                ];
-            };break;
-            case 'console':{
-
-            };break;
-            default:{
-                throw new HttpException(500,'behaviors'.$this->namespace);
-            };break;
-        }
-
-        return $behaviors;
-
-    }
-
+ 
+    public function table(){
+		return $this->table;
+	}
     public function registerTranslations()
     {
         Yii::$app->i18n->translations['site/*'] = [
@@ -68,28 +39,6 @@ class Module extends \kouosl\base\Module
         ];
     }
 
-    public static function t($category, $message, $params = [], $language = null)
-    {
-        return Yii::t('forms/'. $category, $message, $params, $language);
-    }
-
-    public static function initRules(){
-
-        return $rules = [
-            [
-                'class' => 'yii\rest\UrlRule',
-                'controller' => [
-                   'forms/formss',
-                ],
-                'tokens' => [
-                    '{id}' => '<id:\\w+>'
-                ],
-                /*'patterns' => [
-                    'GET new-action' => 'new-action'
-                ]*/
-            ],
-
-        ] ;
-
-    }
+    
+ 
 }
