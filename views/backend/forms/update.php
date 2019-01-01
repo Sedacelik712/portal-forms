5acec137f395adbed325aa0d5c9bde433ebbb829
Sedacelik712 <38867574+Sedacelik712@users.noreply.github.com>
2019-01-01 19:14:33 +0300

    Update update.php

--- views/backend/forms/update.php
+++ views/backend/forms/update.php
@@ -1,21 +1,16 @@
 <?php
 
 use yii\helpers\Html;
+$this->title = Yii::t('app', 'form update') ;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Forms list') , 'url' => ['index']];
+$this->params['breadcrumbs'][] =  $this->title;
 
-/* @var $this yii\web\View */
-/* @var $model kouosl\forms\models\Forms */
-
-$this->title = 'Update Forms: ' . $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Forms', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->form_id]];
-$this->params['breadcrumbs'][] = 'Update';
+echo \kouosl\forms\FormBuilder::widget([
+	'table' => 'poll_',     // 'form_' . $id
+	'config' => [
+			'get'=> true, 
+			'save'=> true, 
+			'autosave' => true,
+		]
+]);
 ?>
-<div class="forms-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
-</div>
