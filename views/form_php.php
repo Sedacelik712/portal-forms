f1cf9cfa87baaec53390be8d5693f58f8c95ac6a
Sedacelik712 <38867574+Sedacelik712@users.noreply.github.com>
2019-01-05 17:48:08 +0300

    Add files via upload

---
+++ views/form_php.php
@@ -0,0 +1,23 @@
+<?php
+use yii\bootstrap\ActiveForm;
+use kouosl\forms\widgets\Form;
+
+$form = ActiveForm::begin();
+	foreach ($form_body as $key => $row) {
+		echo ('<div class="row">');
+
+		    foreach ($row as $key => $value) {
+				echo count($form_body) != 0 ? Form::field($form, $model, $value) : '';
+		    }
+		echo('</div>');
+	}
+
+ActiveForm::end();
+
+$this->registerCss("
+    .ql-align-center { text-align:center }, 
+    .ql-align-right { text-align:right }, 
+");
+?>
+
+
